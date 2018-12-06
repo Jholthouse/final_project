f2c4a09f5c3f1ad22ecfe23216aa75a8c54550ec
Justin Holthouse <justin.holthouse@sjsu.edu>
2018-12-06 15:17:19 -0800

    updated

---
+++ sketch1.js
@@ -0,0 +1,20 @@
+var capture;
+
+function setup() {
+  createCanvas(675,475);
+  capture=createCapture(VIDEO);
+  capture.size(300,200);
+  capture.hide();
+}
+
+function draw() {
+  background(255,0,0);
+  //top row
+  image(capture,25,25,300,200);
+  image(capture,350,25,300,200);
+  //bottom row
+	image(capture,25,250,300,200);
+  image(capture,350,250,300,200);
+  filter(INVERT);
+  filter(THRESHOLD);
+}
